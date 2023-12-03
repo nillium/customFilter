934b4437daa39aaca8ebbd00cb2f219c9a55b1cd
Nillium <theclichehunter@gmail.com>
2023-12-03 08:51:18 +0100

    for rev

--- mainGUI2.py
+++ mainGUI2.py
@@ -1,36 +1,28 @@
 import sys, os
-from PyQt6.QtWidgets import QWidget, QPushButton, QFrame, QGridLayout, QLabel, QFileDialog, QDial, QHBoxLayout, QVBoxLayout, QApplication, QSlider, QGraphicsScene, QGraphicsScene, QGraphicsView, QMainWindow
+from PyQt6.QtWidgets import QWidget, QPushButton, QFrame, QGridLayout, QLabel, QFileDialog, QDial, QGraphicsPixmapItem, QHBoxLayout, QGraphicsTextItem, QGraphicsView, QVBoxLayout, QGraphicsRectItem, QApplication, QSlider, QGraphicsScene, QGraphicsScene, QGraphicsView, QMainWindow
 from PyQt6.QtCore import Qt, QRectF
 from PyQt6.QtGui import QPixmap, QColor, QPen, QImage
 from matplotlib.backend_bases import FigureCanvasBase
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 import numpy as np
 import cv2
-import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 import keras.backend as K
 import numpy as np
-from keras import Input, layers, initializers
-from keras.models import Model
+from keras import layers, initializers
+from keras.layers import Dense, Conv2D, InputLayer, MaxPooling2D, Flatten
+from keras.models import Model, Sequential
 from matplotlib import pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
-from PyQt6.QtWidgets import QApplication, QGraphicsScene, QGraphicsView, QGraphicsRectItem
-from PyQt6.QtCore import Qt
-import sys
-from PyQt6.QtWidgets import QApplication, QGraphicsScene, QGraphicsView, QGraphicsRectItem, QGraphicsTextItem, QGraphicsEllipseItem, QGraphicsPixmapItem
-from PyQt6.QtGui import QPixmap, QColor
-from PyQt6.QtCore import Qt, QRectF
-import sys
 
-class makeLabel(QLabel):
+class make_image_container(QLabel):
     def __init__(self, label_text=None, alignment="center", image_source_type="file", image_source=None, x=0, y=0, w=128, h=32):
-        
         super().__init__()
 
         self.setGeometry(x, y, w, h)
-        self.setStyleSheet("background-color: lightgray; border: 1px solid #cccccc; padding: 0px;")
+        self.setStyleSheet("background-color: lightgray; border: 1px solid #bbbbbb; padding: 0px;")
         
         if not label_text == None:
             self.setText(label_text)
@@ -85,7 +77,23 @@ class makeLabel(QLabel):
         self.setGeometry(x, y, w, h)
         self.scene.setBackgroundBrush(QColor(r, g, b, a))  # Set the background color to light gray
         self.setParent(main_window.main_widget) """
+class devmod:
+    def __init__(self):
+        super().__init__()
+        
+    
+    def model(self, dtype=None):
+        uptpdate_values = contents.update_values()
+        model = Sequential()
+        model.add(InputLayer(input_shape=(512, 512, 1)))
+        model.add(Conv2D(kernel_size=(uptpdate_values[0], uptpdate_values[0]), kernel_initializer=self.initialize_kernel))
+        model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+        return model
 
+    def initialize_kernel(self, shape, dtype=None):
+        customFilter = customFilter.reshape((self.filterSize, self.filterSize, 1, 1))
+        assert customFilter.shape == shape
+        return K.variable(customFilter, dtype='float32')
 
 class makeSlider(QSlider):
     def __init__(self, label, call_on_change, x=0,y=0,w=512,h=32,min=0,max=100,val=50):
@@ -171,10 +179,10 @@ class Contents:
         #self.leftCanvas = makeCanvas(x=10, y=10, r=200, a=0)
         #self.centerCanvas = makeCanvas(x=532, y=10,w=256,h=256)
         #self.rightCanvas = makeCanvas(x=798, y=10,w=512,h=512)^
-        self.label_left = makeLabel(image_source_type="file", image_source="checker.png", x=10, y=10, w=512, h=512)
-        self.label_center_top = makeLabel(alignment="left", image_source_type="file", image_source="checker.png", x=532, y=10, w=256, h=256)
-        self.label_center_bot = makeLabel(alignment="left", image_source_type="file", image_source="checker.png", x=532, y=316+64+10+38, w=256, h=256)
-        self.label_right = makeLabel(image_source_type="file", image_source="checker.png", x=798, y=10, w=512, h=512)
+        self.image_container_left = make_image_container(image_source_type="file", image_source="checker.png", x=10, y=10, w=512, h=512)
+        self.image_container_center_top = make_image_container(alignment="left", image_source_type="file", image_source="checker.png", x=532, y=10, w=256, h=256)
+        self.image_container_center_bot = make_image_container(alignment="left", image_source_type="file", image_source="checker.png", x=532, y=316+64+10+38, w=256, h=256)
+        self.image_container_right = make_image_container(image_source_type="file", image_source="checker.png", x=798, y=10, w=512, h=512)
         
         self.change_input_BTN = makeButton("Change Input", self.change_input_press, x=10, y=532)
 
@@ -189,8 +197,8 @@ class Contents:
         #self.convolute_BTN = makeButton("--> Convolute -->", self.convolute_press, x=532, y=276+64+42+42, w=256)
 
         self.filter_size_DIAL = makeDial(self.filter_size_DIAL_value_change, 532, 316, 64, 64, 2, 64, 32)
-        self.white_treshold_DIAL = makeDial(self.white_treshold_DIAL_value_change, 532+64, 316, 64, 64, 0, 126, 64)
-        self.black_treshold_DIAL = makeDial(self.black_treshold_DIAL_value_change, 532+128, 316, 64, 64, 0, 126, 255)
+        self.white_treshold_DIAL = makeDial(self.white_treshold_DIAL_value_change, 532+64, 316, 64, 64, 0, 255, 255)
+        self.black_treshold_DIAL = makeDial(self.black_treshold_DIAL_value_change, 532+128, 316, 64, 64, 0, 255, 0)
         
         #self.slider.sliderlabel.setParent(main_window.main_widget)
         #self.slider.valueChanged.connect()
@@ -201,78 +209,35 @@ class Contents:
        
        
         self.items={
-            "label_left" : self.label_left,
-            "label_center_top" : self.label_center_top,
-            "label_center_bot" : self.label_center_bot
+            "image_container_left" : self.image_container_left,
+            "image_container_center_top" : self.image_container_center_top,
+            "image_container_center_bot" : self.image_container_center_bot
             #"filter_size_SLDR" : self.filter_size_SLDR
         }
-    
 
-    
-    
-    def change_image(self, label_name, random=True, new=False, process=True, invert=False, filter_size=8, white_treshold=192, black_treshold=64):
-        
-        self.display_width = self.items[label_name].width()
-        self.display_heigth = self.items[label_name].height()
+        self.random_filter_press()
 
-        if not random and new and not process:
-            file_type_filter = 'Image File (*.png *.jpg)'
-            response = QFileDialog.getOpenFileName(
-                parent=main_window.main_widget,
-                caption='Select a file',
-                directory=os.getcwd(),
-                filter=file_type_filter,
-                initialFilter='Image File (*.png *.jpg)')
-            file_dialog = QFileDialog()
-            file_dialog.setWindowTitle('Open File')
-            #HANDLE EMPTY selection!
-            print(f'Selected File: {response[0]}')
-            image_path = response[0]
-            self.image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-            self.image = cv2.resize(self.image, (self.display_width,self.display_heigth))
-        if random and new and not process:
-            self.image = np.random.randint(0, 256, size=(filter_size, filter_size), dtype=np.uint8)
-            self.image = cv2.resize(self.image, (self.display_width,self.display_heigth), interpolation = cv2.INTER_NEAREST)
-            
-        
-        if not random and not new and process:
-
-            if invert:
-                self.image = cv2.bitwise_not(self.image)
-            
-            self.image = cv2.resize(self.image, (self.display_width,self.display_heigth), interpolation = cv2.INTER_NEAREST)
-            
-            ret1,self.image = cv2.threshold(self.image,0,self.black_treshold_DIAL.value(),cv2.THRESH_BINARY)
-                      
-            ret1,self.image_up = cv2.threshold(self.image,0,self.white_treshold_DIAL.value(),cv2.THRESH_BINARY)
-
-            ret1,self.image_down = cv2.threshold(self.image,0,self.black_treshold_DIAL.value(),cv2.THRESH_BINARY)
-        if black_treshold_change and not random and not invert:
-            pass
-
-
-        
-        self.display_image = QImage(self.image, self.display_width, self.display_heigth, (1*self.display_width), QImage.Format.Format_Grayscale8)
-            
-        self.display_pixmap = QPixmap.fromImage(self.display_image)
-        self.items[label_name].setPixmap(self.display_pixmap)
-        
     def change_input_press(self):
         print("change_input_press")
-        self.change_image("label_left")
+        self.load_custom_image(filter=False, input=True)
+        self.display("image_container_left", self.input_original, 1)
     
     def custom_filter_press(self):
         print("custom_filter_press")
-        self.change_image("label_center_top", random=False, new=True)
+        self.load_custom_image(filter=True, input=False)
+        self.display("image_container_center_top", self.filter_original, 2)
 
     def random_filter_press(self):
         print("random_filter_press")
-        
-        self.change_image("label_center_top", random=True, new=True, process=False, filter_size=self.filter_size_DIAL.value())
+        self.update_values()
+        self.randomize(self.filter_size)
+        self.display("image_container_center_top", self.filter_original, 2)
     
     def invert_filter_press(self):
         print("invert_filter_press")
-        self.change_image("label_center_bot", invert=True, random=False, filter_update=True, filter_size=self.filter_size_DIAL.value())
+        self.invert()
+        self.display("image_container_center_bot", self.filter_original, 2)
+        #self.change_image("label_center_bot", invert=True, random=False, filter_update=True, filter_size=self.filter_size_DIAL.value())
 
     def convolute_press(self):
         print("convolute_press")
@@ -284,6 +249,10 @@ class Contents:
         print("filter_size_DIAL_value_change")
     
     def white_treshold_DIAL_value_change(self):
+        self.update_values()
+        print(self.white_treshold)
+        self.trim_from_top(self.white_treshold)
+        self.display("image_container_center_bot", self.filter_trimmed_from_top, 2)
         print("white_treshold_DIAL_value_change")
     
     def black_treshold_DIAL_value_change(self):
@@ -291,10 +260,77 @@ class Contents:
     
     def process_filter_press(self):
         print("process_filter_press")
-        self.change_image("label_center_bot", filter_size=self.filter_size_DIAL.value(), white_treshold=self.white_treshold_DIAL.value(), black_treshold=self.black_treshold_DIAL.value()d )
+        self.update_values()
+        print(self.black_treshold)
+        self.trim_from_top(self.white_treshold)
+        self.trim_from_bottom(self.white_treshold)
+        self.display("image_container_center_bot", self.filter_original, 2)
 
-
+    def update_values(self):
+        self.filter_size = contents.filter_size_DIAL.value()
+        self.white_treshold = contents.white_treshold_DIAL.value()
+        self.black_treshold = contents.black_treshold_DIAL.value()
+        return self.filter_size, self.white_treshold, self.black_treshold
         
+    def load_custom_image(self, filter=True, input=False):
+        file_type_filter = 'Image File (*.png *.jpg)'
+        response = QFileDialog.getOpenFileName(
+            parent=main_window.main_widget,
+            caption='Select a file',
+            directory=os.getcwd(),
+            filter=file_type_filter,
+            initialFilter='Image File (*.png *.jpg)')
+        file_dialog = QFileDialog()
+        file_dialog.setWindowTitle('Open File')
+        #HANDLE EMPTY selection!
+        print(f'Selected File: {response[0]}')
+        image_path = response[0]
+        if filter:
+            self.filter_original = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+        if input:
+            self.input_original = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+
+    def randomize(self, filter_size):
+        self.filter_original = np.random.randint(0, 256, size=(filter_size, filter_size), dtype=np.uint8)
+
+    def invert(self):
+        self.filter_original = cv2.bitwise_not(self.filter_original)
+        
+    def trim_from_top(self, white_treshold):
+        ret,self.filter_trimmed_from_top = cv2.threshold(self.filter_original,white_treshold,255,cv2.THRESH_TRUNC)
+        #ret1,self.filter_original = cv2.threshold(self.filter_original,0,self.white_treshold,cv2.THRESH_BINARY)
+
+    def trim_from_bottom(self, black_treshold):
+        ret,self.filter_trimmed_from_bottom = cv2.threshold(self.filter_original,0,black_treshold,cv2.THRESH_TRUNC)
+        #ret1,self.filter_original = cv2.threshold(self.filter_original,0,black_treshold,cv2.THRESH_BINARY)
+
+    def display(self,  image_container_name, image, image_category=1):
+        display_width = contents.items[image_container_name].width()
+        display_height = contents.items[image_container_name].height()
+        if image_category == 2:
+            filter_to_display = cv2.resize(image, (display_width,display_height), interpolation = cv2.INTER_NEAREST)
+            filter_to_display_pix = QImage(filter_to_display, display_width, display_height, (1*display_width), QImage.Format.Format_Grayscale8)
+            self.filter_display_pixmap = QPixmap.fromImage(filter_to_display_pix)
+            contents.items[image_container_name].setPixmap(self.filter_display_pixmap)
+        if image_category == 1:
+            input_to_display = cv2.resize(image, (display_width,display_height), interpolation = cv2.INTER_NEAREST)
+            input_to_display_pix = QImage(input_to_display, display_width, display_height, (1*display_width), QImage.Format.Format_Grayscale8)
+            self.input_display_pixmap = QPixmap.fromImage(input_to_display_pix)
+            contents.items[image_container_name].setPixmap(self.input_display_pixmap)
+        if image_category == 3:
+            output_to_display = cv2.resize(image, (display_width,display_height), interpolation = cv2.INTER_NEAREST)
+            output_to_display_pix = QImage(output_to_display, display_width, display_height, (1*display_width), QImage.Format.Format_Grayscale8)
+            self.output_display_pixmap = QPixmap.fromImage(output_to_display_pix)
+            contents.items[image_container_name].setPixmap(self.output_display_pixmap)
+        if image_category == None:
+            print("No image name defined. Define image name as, input_original or filter_original or output_original.")
+        
+
+
+
+
+
+
         #self.change_image("label_center_bot", invert=False, random=False, black_treshold_change=True, filter_update=True, filter_size=self.filter_size_DIAL.value())
         #ret2,minus = cv2.threshold(self.customfilter,self.lowerTreshold,127,cv2.THRESH_BINARY)
 
@@ -634,7 +670,7 @@ class mainWindow(QWidget):
     def build_model(self):
         inputs = tf.keras.Input(shape=(512, 512, 1))
         convolute = layers.Conv2D(filters=1, kernel_size = self.filterSize, kernel_initializer=self.my_filter, strides=1, padding='valid') (inputs)
-        #flat = layers.Flatten()(x)
+        #flat = layers.Flatten()(convolute)
         #relu = layers.Dense(128, activation=tf.nn.relu)(flat)
         #dense = layers.Dense(2, activation=tf.nn.softmax)(relu)
         self.model = Model(inputs=inputs, outputs=convolute)
